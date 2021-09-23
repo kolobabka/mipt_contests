453d531c89987c038cb6d96b69aa76ae7d44f3a8
belov.vv@phystech.edu <belov.vv@phystech.edu>
2021-09-23 14:36:13 +0300

    Don't breathe...

--- 3_sem/JadHW/3_th/NewVersion/bizzbuzz.c
+++ 3_sem/JadHW/3_th/NewVersion/bizzbuzz.c
@@ -54,59 +54,59 @@ signed char PerrorCheck (int file) {
 //-----------------------------------------------------------
 void BizzBuzzer (int file_1, int file_2, char** argv) {
 
-    long long totLenght  = 0ULL;
-    long long lenght     = 0ULL;
+    long long totlength  = 0ULL;
+    long long length     = 1ULL;
 
     char rBuffer[RSIZE] = {0};
     char wBuffer[WSIZE] = {0};
     
-    long long wBufLenght = 0;
+    long long wBuflength = 0;
 
-    totLenght = SizeText (argv[1]);   
+    totlength = SizeText (argv[1]);   
     
-    printf ("%ld\n", totLenght);
+    printf ("%lld\n", totlength);
 
-    while (totLenght > 0) {
+    while (totlength > 0 && length > 0) {
 
-        lenght = read (file_1, rBuffer, RSIZE);
-        if (PerrorCheck (lenght) == 0) 
+        length = read (file_1, rBuffer, RSIZE);
+        if (PerrorCheck (length) == 0) 
             return;
 
-        lenght = SepParsNumbers (file_1, file_2, rBuffer, wBuffer, lenght, totLenght);
-        totLenght -= lenght;
-        printf ("totLenght = %ld\n", totLenght);
+        length = SepParsNumbers (file_1, file_2, rBuffer, length, totlength);
+        totlength -= length;
+        printf ("totlength = %lld\n", totlength);
     }
 
 }
 //-----------------------------------------------------------
 //-----------------------------------------------------------
-long long SepParsNumbers (int file_1, int file_2, const char* rBuffer, char* wBuffer, const long long lenght, const long long totLenght) {
 
-    long long wBufLenght = 0ULL;
-    long long shift      = 0Ull;
-    long long index      = 0ULL;
-    int descriptor    = 0;
-    int lastNum       = 0;
-    int offset        = 0;
-    int sum           = 0;
+int SepParsNumbers (int file_1, int file_2, const char* rBuffer, long long length, long long totLength) {
     
-    char symbol       = 0;
+    int descriptor  = 0;
+    long long shift = 0;
+    long long index = 0;
+    int sum         = 0;
+    int offset      = 0;
+    int lastNum     = 0;
 
-    while (index < lenght) {
+    while (index < length) {
 
         if (isspace (rBuffer[index])) {
 
             descriptor = write (file_2, rBuffer + index, 1);
             if (PerrorCheck (descriptor) == 0) 
                 return 1;
-            ++index;
             
+            ++index;
             continue;
         }
 
-        
-        if (index < lenght && isdigit (rBuffer[index])) {
+        if (isdigit (rBuffer[index])) {
 
+            descriptor = write (file_2, rBuffer + index, 1);
+            if (PerrorCheck (descriptor) == 0) 
+                return 1;
             lastNum = rBuffer[index] - '0';
             sum += lastNum;
             sum %= 3;
@@ -114,152 +114,80 @@ long long SepParsNumbers (int file_1, int file_2, const char* rBuffer, char* wBu
             ++index;
         }
 
-        if (index >= lenght) {
-
-            index = ToFile (file_1, file_2, shift, index - shift, totLenght);
-            return lenght + index;
-        }
-
-        if (isspace (rBuffer[index])) {
-
-            NumCases (rBuffer, sum, lastNum, index, shift, file_1, file_2);
-            shift = 0;
-            continue;
-        }
-
-        if (!(isdigit (rBuffer[index]))) {
+        if (index >= length) {
             
-            offset = lseek (file_1, -(lenght - index + shift), SEEK_CUR);
+            long long lastShift = 0;
+            char symb = rBuffer[index - 1];
+            lastShift = shift;
 
-            shift = WordWrite (file_1, file_2, shift);
-            index += shift;
+            if (isspace (rBuffer[index])) {
+
+                NumCases (rBuffer, sum, lastNum, shift, file_1, file_2);  
+                shift = 0; 
+                sum = 0;
+                return index;
+            }
+
+            while (!(isspace(symb))) {
+                
+                descriptor = read (file_1, &symb, 1);
+                if (PerrorCheck (descriptor) == 0) 
+                    return 1;
+
+                descriptor = write (file_2, &symb, 1); //Maybe a bit later???
+                if (PerrorCheck (descriptor) == 0) 
+                    return 1;
+                
+                ++shift;
+
+                if (isspace (symb)) {
+
+                    offset = NumCases (rBuffer, sum, lastNum, shift, file_1, file_2);  
+                    if (offset == 0) {
+                        
+                        descriptor = write (file_2, &symb, 1); //Maybe a bit later???
+                        if (PerrorCheck (descriptor) == 0) 
+                            return 1;
+                    }
+
+                    return index + shift - lastShift;
+                }
+                if (isdigit (symb)) {
+
+                    lastNum = symb - '0';
+                    sum += lastNum;
+                    sum %= 3;
+                    // ++shift;
+                    // ++index;
+                }
+            }
         }
 
+        if (index < length && isspace (rBuffer[index])) {
+
+            NumCases (rBuffer, sum, lastNum, shift, file_1, file_2);  
+            shift = 0; 
+            sum = 0;
+            continue;
+        }  
+        
     }
-    
 
     return index;
 }
-//-----------------------------------------------------------
-//-----------------------------------------------------------
-int ToFile (int file_1, int file_2, long long shift, long long index, const long long totLenght) {
 
+long long NumCases (const char* rBuffer, const int sum, const int lastNum, long long shift, int file_1, int file_2) {
 
-    // RETURN CORRECT SHIFT!
-    char symb         = ' ';
-    int descriptor    = 0;
-    int offset        = 0;
-    int lastNum       = 0;
-    int sum           = 0;
-    int prevShift     = 0;
+    int descriptor = 0;
+    int offset     = 0;
+    assert (rBuffer);
 
-    prevShift = shift;
-
-    offset = lseek (file_1, -shift, SEEK_CUR);
+    offset = lseek (file_2, -shift, SEEK_CUR);
     if (offset == 1L) {
 
         printf ("Error!\n");
         return 1;
     }
-
-    shift = -shift - 1;
-
-    while (index < totLenght) {
-
-        descriptor = read (file_1, &symb, 1);
-        if (PerrorCheck (descriptor) == 0) 
-            return 1;
-        
-        if (index < totLenght && isdigit (symb)) {
-
-            lastNum = symb - '0';
-            sum += lastNum;
-            sum %= 3;
-            ++shift;
-            ++index;
-        }
-
-        if (isspace (symb)) {
-            
-            if (((sum % 3) == 0) && (lastNum == 5 || lastNum == 0)) {
-
-                descriptor = write (file_2, "BizzBuzz", bizzbuzzLen);
-                if (PerrorCheck (descriptor) == 0) 
-                    return 1;
-
-                return shift;
-            }
-
-            if ((lastNum == 5 || lastNum == 0)) {
-
-                descriptor = write (file_2, "Buzz", buzzLen);
-                if (PerrorCheck (descriptor) == 0) 
-                    return 1;
-
-                return shift;
-            }
-
-            if ((sum % 3) == 0) {
-
-                descriptor = write (file_2, "Bizz", bizzLen);
-                if (PerrorCheck (descriptor) == 0) 
-                    return 1;
-
-                return shift;
-            }
-
-            offset = lseek (file_1, -(shift + prevShift), SEEK_CUR);
-            if (offset == 1L) {
-
-                printf ("Error!\n");
-                return 1;
-            }
-
-            for (int i = 0; i < shift + prevShift; i++) {
-
-                descriptor = read (file_1, &symb, 1);
-                if (PerrorCheck (descriptor) == 0) 
-                    return 1;
-
-                descriptor = write (file_2, &symb, 1);
-                if (PerrorCheck (descriptor) == 0) 
-                    return 1;
-            }
-
-            return shift;
-        }
-        if (!(isdigit(symb))) {
-            
-            offset = lseek (file_1, -(shift + prevShift + 1), SEEK_CUR);
-            if (offset == 1L) {
-
-                printf ("Error!\n");
-                return 1;
-            }
-
-            shift = 0;
-            while (!(isspace(symb))) {
-                ++shift;
-                descriptor = read (file_1, &symb, 1);
-                if (PerrorCheck (descriptor) == 0) 
-                    return 1;
-
-                descriptor = write (file_2, &symb, 1);
-                if (PerrorCheck (descriptor) == 0) 
-                    return 1;
-            }
-            return (shift - prevShift); 
-        }
-        
-    }
-}
-
-//-----------------------------------------------------------
-//-----------------------------------------------------------
-long long NumCases (const char* rBuffer, const int sum, const int lastNum, const long long index, long long shift, int file_1, int file_2) {
-
-    int descriptor = 0;
-    assert (rBuffer);
 
     if (((sum % 3) == 0) && (lastNum == 5 || lastNum == 0)) {
 
@@ -287,29 +215,14 @@ long long NumCases (const char* rBuffer, const int sum, const int lastNum, const
 
         return 0;
     }
-    
-    return (WordWrite (file_1, file_2, shift));
-}
-//-----------------------------------------------------------
-//-----------------------------------------------------------
-long long WordWrite (int file_1, int file_2, long long shift) {
+    offset = lseek (file_2, shift, SEEK_CUR);
+    if (offset == 1L) {
 
-    char symb = 'A';
-    int index = 0;
-    int descriptor = 0;
-
-    shift = -shift;
-
-    while (!(isspace(symb))) {
-        ++shift;
-        descriptor = read (file_1, &symb, 1);
-        if (PerrorCheck (descriptor) == 0) 
-            return 1;
-
-        descriptor = write (file_2, &symb, 1);
-        if (PerrorCheck (descriptor) == 0) 
-            return 1;
+        printf ("Error!\n");
+        return 1;
     }
-    return (shift); 
+    return -1;
 
+
+    // return (WordWrite (file_1, file_2, shift));
 }
