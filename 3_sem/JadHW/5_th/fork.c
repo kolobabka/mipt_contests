0c90ab9ddaa649214a23c8c322aabb0f96756690
belov.vv@phystech.edu <belov.vv@phystech.edu>
2021-09-30 17:33:00 +0300

    Add Pid-task

--- 3_sem/JadHW/5_th/fork.c
+++ 3_sem/JadHW/5_th/fork.c
@@ -19,62 +19,65 @@
 signed char PerrorCheck (int file);
 void Inverse (char* buf, int index);
 
+const int size = 1024;
+
 int main () {
 
     pid_t pid = 0;  
     int file = 0;
     int err = 0;
     char buf[100] = {0};
-    char pBuf[100] = {0};
     
 
     file = open ("Pids", O_WRONLY | O_CREAT | O_TRUNC, 0777);
     if ((PerrorCheck (file)) == 0)
         return -1;
 
-    for (int i = 0; i < 1024; i++) {
+    for (int i = 0; i < size; i++) {
+
+        pid_t retVal   = 0;
+        int waitstatus = 0;
+        int args       = 0;
 
         pid = fork ();
-        
+        if (pid == -1) {
+
+            printf ("Pid error!");
+            close (file);
+            return -1;
+        }
     
 
         if (pid == 0) {
-
-            pid_t gottenPid = 0;
-            pid_t pPid = 0;
-            gottenPid = getpid ();
-             
             
-            printf ("PPID = %d\n", pPid);
-            int index = 0;
-            int pIndex = 0;
+            args = sprintf (buf, "MyPid = %d\nParentPid = %d\n", getpid (), getppid ());
 
-            index = sprintf (buf, "%d", gottenPid);
+            err = write (file, buf, args);
+            if ((PerrorCheck (err)) == 0)
+                return -1;
 
-            buf[index] = '\n';
-
-            err = write (file, "pid ", strlen ("pid "));
-            err = write (file, buf, index + 1);
-
-            pIndex = sprintf (pBuf, "%d", pPid);
-
-            
-
-            err = write (file, "ppid ", strlen ("ppid "));
-
-            pBuf[pIndex] = '\n';
-
-            err = write (file, pBuf, pIndex + 1);
-            err = write (file, " ", 1);
-
-            return 0;
+            return 0;     
         }
 
-        else {
-            
-            
+        printf ("PPid = %d\n", pid);
+
+        retVal = waitpid (pid, &waitstatus, 0);
+
+        if (!WIFEXITED (waitstatus) || WEXITSTATUS(waitstatus))
+        {
+            close (file);
+            return -1;
         }
-        
+
+        args = sprintf (buf, "ChildPid = %d\n", retVal);
+
+        err = write (file, buf, args);
+        if ((PerrorCheck (err)) == 0) {
+
+            close (file);
+            return -1; 
+        }
+            
     }
 
 
@@ -91,23 +94,4 @@ signed char PerrorCheck (int file) {
     }
 
     return 1;
-}
-
-void Inverse (char* buf, int index) {
-
-    char* tmp = NULL;
-
-    tmp = (char*) calloc (index, sizeof (char));
-
-    for (int i = 0; i < index; i++) {
-
-        tmp[i] = buf[index - i]; 
-    }
-
-    for (int i = 0; i < index; i++) {
-
-        buf[i] = buf[i]; 
-    }
-
-    free (tmp);
 }
